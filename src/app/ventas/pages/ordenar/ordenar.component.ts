9c3e32c7b9327ec4d232785326e895d83c02b94b
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-28 19:20:23 -0500

    Pipe personalizado

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class OrdenarComponent {
 
+  enMayusculas: boolean = false;
+
+  constructor(){}
+
+  cambiarMayuscula(){
+    this.enMayusculas = !this.enMayusculas;
+  }
 }
